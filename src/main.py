da339aecfa0ea7e5e01f4d34f8f5e0c5832331b0
Ben Smith <benjamin.smith@fdbhealth.com>
2024-03-27 09:56:15 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,28 @@
 import zipfile
 import requests
+import os
 
 DOWNLOAD_URL = "https://www.fda.gov/media/76860/download?attachment"
-DOWNLOAD_FILE = "orange_book.zip"
-EXTRACT_DIR = "orange_book"
+DOWNLOAD_FILE = "orange_book_data.zip"
+EXTRACT_DIR = "orange_book_data"
 FILENAMES = ["exclusivity.txt", "patent.txt", "products.txt"]
 
 # https://www.fda.gov/drugs/drug-approvals-and-databases/orange-book-data-files
-print("Downloading file")
-response = requests.get("https://www.fda.gov/media/76860/download?attachment")
+print(f"Downloading file {DOWNLOAD_URL}")
+response = requests.get(DOWNLOAD_URL)
 
-print("Writing to disk")
-with open("orange_book.zip", "wb") as f:
+print(f"Writing file to disk {DOWNLOAD_FILE}")
+with open(DOWNLOAD_FILE, "wb") as f:
     f.write(response.content)
 
 print("Extracting files")
-with zipfile.ZipFile("orange_book.zip", "r") as z:
-    z.extractall("orange_book")
+with zipfile.ZipFile(DOWNLOAD_FILE, "r") as z:
+    z.extractall(EXTRACT_DIR)
 
 print("Processsing files")
 for filename in FILENAMES:
-    print(f"Reading {filename}")
-    with open(f"{EXTRACT_DIR}/{filename}", "r") as f:
+    file_path = os.path.join(EXTRACT_DIR, filename)
+    print(f"Reading file {file_path}")
+    with open(file_path, "r") as f:
         num_lines = sum(1 for line in f)
         print(f"Number of lines: {num_lines}")
